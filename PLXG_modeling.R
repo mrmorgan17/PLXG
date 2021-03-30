553fa7c2e8228bd217f65bec7985b1174670c039
Matthew Morgan <matt4byu@gmail.com>
2021-03-30 10:50:38 -0600

    Cleaning up the PLXG_modeling.R file

--- PLXG_modeling.R
+++ PLXG_modeling.R
@@ -12,6 +12,9 @@ pl_team_data <- vroom('Pl_team_match_data.csv') %>% mutate(Dist = ifelse(is.na(D
 # Opp_GK_AvgLen = 37 NA's
 # Opp_AvgDist = 24 NA's
 
+# NA's were imputed with 0s
+
+# Splitting the data into a training set and a test set
 split <- sample.split(pl_team_data$Goals, SplitRatio = 0.8)
 
 pl_team_data.train <- subset(pl_team_data, split == TRUE)
@@ -19,6 +22,7 @@ pl_team_data.test <- subset(pl_team_data, split == FALSE)
 
 test.Goals <- pl_team_data.test %>% pull(Goals)
 
+# Removing the Goals variable from the test set
 pl_team_data.test <- pl_team_data.test %>% select(-Goals)
  
 # xgbTree model => best so far
@@ -43,11 +47,16 @@ xgbTree.preds <- predict(xgbTree.model, pl_team_data.test)
 xgbTree.rounded.preds <- round(xgbTree.preds)
 xgbTree.rounded.preds <- ifelse(xgbTree.rounded.preds < 0, 0, xgbTree.rounded.preds)
 
+# Calculate RMSE for the test data with normal predictions from xgbTree.model
 sqrt(mean((test.Goals - xgbTree.preds)^2))
 # .3208364
 
+# Calculate RMSE for the test data with rounded predictions from xgbTree.model
 sqrt(mean((test.Goals - xgbTree.rounded.preds)^2))
 # .3247992
+
+# What are the most important varaibles for xgbTree.model?
+varImp(xgbTree.model)
 
 # 15 most important variables for xgbTree.model
 # SoT             0.6082823
@@ -66,37 +75,8 @@ sqrt(mean((test.Goals - xgbTree.rounded.preds)^2))
 # SCA_PassLive    0.0005878
 # Other           0.0005757
 
-
+# Building an xgbTree model with only the 10 most important variables from varImp(xgbTree.model)
 xgbTree2.model <- train(Goals ~ .,
-                        data = pl_team_data.train %>% select(-Date, -Round, -Venue, -Opponent),
-                        method = 'xgbTree',
-                        trControl = trainControl(method = 'cv', number = 10),
-                        tuneGrid = expand.grid(nrounds = c(400, 450, 500, 550),
-                                               max_depth = 1,
-                                               eta = c(.3, .31, .32, .325),
-                                               gamma = 0,
-                                               colsample_bytree = .8,
-                                               min_child_weight = 1,
-                                               subsample = 1),
-                        maximize = FALSE)
-
-xgbTree2.model
-# .32590965
-
-xgbTree2.preds <- predict(xgbTree2.model, pl_team_data.test)
-
-# Predict team-average rows...
-
-xgbTree2.rounded.preds <- round(xgbTree2.preds)
-xgbTree2.rounded.preds <- ifelse(xgbTree2.rounded.preds < 0, 0, xgbTree2.rounded.preds)
-
-sqrt(mean((test.Goals - xgbTree2.preds)^2))
-# .3275023
-
-sqrt(mean((test.Goals - xgbTree2.rounded.preds)^2))
-# .3281651
-
-xgbTree3.model <- train(Goals ~ .,
                         data = pl_team_data.train %>% select(Goals, Team, SoT, Opp_Saves, PKatt, SCA_PassLive, Short_Att, Dist, Clr, TklW, TB, Crosses_Att),
                         method = 'xgbTree',
                         trControl = trainControl(method = 'cv', number = 10),
@@ -109,37 +89,37 @@ xgbTree3.model <- train(Goals ~ .,
                                                subsample = 1),
                         maximize = FALSE)
 
-xgbTree3.model
+xgbTree2.model
 # .3310949
 
-xgbTree3.preds <- predict(xgbTree3.model, pl_team_data.test)
+xgbTree2.preds <- predict(xgbTree2.model, pl_team_data.test)
 
-xgbTree3.rounded.preds <- round(xgbTree3.preds)
-xgbTree3.rounded.preds <- ifelse(xgbTree3.rounded.preds < 0, 0, xgbTree3.rounded.preds)
+xgbTree2.rounded.preds <- round(xgbTree2.preds)
+xgbTree2.rounded.preds <- ifelse(xgbTree2.rounded.preds < 0, 0, xgbTree2.rounded.preds)
 
-sqrt(mean((test.Goals - xgbTree3.preds)^2))
+sqrt(mean((test.Goals - xgbTree2.preds)^2))
 # .3208364
 
-sqrt(mean((test.Goals - xgbTree3.rounded.preds)^2))
+sqrt(mean((test.Goals - xgbTree2.rounded.preds)^2))
 
-# Saving this model as an RDS object
-saveRDS(xgbTree3.model, '/Users/matthewmorgan/Documents/Stat 495R/PLXG App/PLXGModel.RData')
+# Saving this model as an RDS object so it can be used by the Shiny app
+saveRDS(xgbTree2.model, '/Users/matthewmorgan/Documents/Stat 495R/PLXG/PLXG App/PLXGModel.RData')
 
 # Poisson Regression
 summary(m1 <- glm(Goals ~ ., family = 'poisson', data = pl1920.train))
 
-m1.preds <- predict(m1, pl1920.test)
+m1.preds <- predict(m1, pl_team_data.test)
 
 m1.rounded.preds <- round(m1.preds)
 m1.rounded.preds <- ifelse(m1.rounded.preds < 0, 0, m1.rounded.preds)
 
-sum(test.Goals - m1.rounded.preds)/length(test.Goals)
+sqrt(mean((test.Goals - m1.preds)^2))
 
-# Using Lasso regression for variable selection
+sqrt(mean((test.Goals - m1.rounded.preds)^2))
 
 # ranger Model
 ranger.model <- train(Goals ~ ., 
-                      data = pl1920.train,
+                      data = pl_team_data.train,
                       method = 'ranger',
                       trControl = trainControl(method = 'cv', number = 10),
                       preProcess = c('nzv'),
@@ -151,16 +131,18 @@ ranger.model <- train(Goals ~ .,
 
 ranger.model
 
-ranger.preds <- predict(ranger.model, pl1920.test)
+ranger.preds <- predict(ranger.model, pl_team_data.test)
 
 ranger.rounded.preds <- round(ranger.preds)
 ranger.rounded.preds <- ifelse(ranger.rounded.preds < 0, 0, ranger.rounded.preds)
 
-sum(sqrt((test.Goals - ranger.rounded.preds)^2))/length(test.Goals)
+sqrt(mean((test.Goals - ranger.preds)^2))
+
+sqrt(mean((test.Goals - ranger.rounded.preds)^2))
 
 # randomForest Model
 rf.model <- train(Goals ~ .,
-                  data = pl1920.train,
+                  data = pl_team_data.train,
                   method = 'rf',
                   trControl = trainControl(method = 'cv', number = 10),
                   preProcess = c('nzv'),
@@ -170,16 +152,18 @@ rf.model <- train(Goals ~ .,
 
 rf.model
 
-rf.preds <- predict(rf.model, pl1920.test)
+rf.preds <- predict(rf.model, pl_team_data.test)
 
 rf.rounded.preds <- round(rf.preds)
 rf.rounded.preds <- ifelse(rf.rounded.preds < 0, 0, rf.rounded.preds)
 
-sum(sqrt((test.Goals - rf.rounded.preds)^2))/length(test.Goals)
+sqrt(mean((test.Goals - rf.preds)^2))
+
+sqrt(mean((test.Goals - rf.rounded.preds)^2))
 
 # xgbLinear model
 xgbLinear.model <- train(Goals ~ .,
-                         data = pl1920.train,
+                         data = pl_team_data.train,
                          method = 'xgbLinear',
                          trControl = trainControl(method = 'cv', number = 10),
                          tuneGrid = expand.grid(nrounds = 100,
@@ -189,6 +173,15 @@ xgbLinear.model <- train(Goals ~ .,
                          maximize = FALSE)
 
 xgbLinear.model
+
+xgbLinear.preds <- predict(xgbLinear.model, pl_team_data.test)
+
+xgbLinear.rounded.preds <- round(xgbLinear.preds)
+xgbLinear.rounded.preds <- ifelse(xgbLinear.rounded.preds < 0, 0, xgbLinear.rounded.preds)
+
+sqrt(mean((test.Goals - xgbLinear.preds)^2))
+
+sqrt(mean((test.Goals - xgbLinear.rounded.preds)^2))
 
 # Getting Team Averages
 Pl_team_avg <- pl_team_data %>% select(-Goals, -Date, -Round, -Venue, -Opponent) %>% group_split(Team)
@@ -202,16 +195,11 @@ for (i in 1:length(Pl_team_avg)) {
 
 Avg_Dat <- do.call(rbind, Avg_Dat)
 
+# Putting the Team column first
 Avg_Dat <- Avg_Dat[,c(98, 1:97)]
 
+# Select the Team and the 10 most important variables
 Avg_Dat_10 <- Avg_Dat %>% select(Team, SoT, Opp_Saves, PKatt, SCA_PassLive, Short_Att, Dist, Clr, TklW, TB, Crosses_Att)
 
-write.csv(Avg_Dat_10, '/Users/matthewmorgan/Documents/Stat 495R/PLXG App/PL_10.csv', row.names = FALSE)
-
-Arsenal3.preds <- predict(xgbTree3.model, Avg_Dat_10[1,])
-Arsenal2.preds <- predict(xgbTree2.model, Avg_Dat[1,])
-Arsenal.preds <- predict(xgbTree.model, Avg_Dat[1,])
-
-varImp(xgbTree3.model)
-
-saveRDS(xgbTree3.model, '/Users/matthewmorgan/Documents/Stat 495R/PLXG App')
+# Writing out a .csv of each team's averages for the 10 most important variables for xgbTree3.model to be used in the Shint App
+write.csv(Avg_Dat_10, '/Users/matthewmorgan/Documents/Stat 495R/PLXG/PLXG App/PL_10.csv', row.names = FALSE)
