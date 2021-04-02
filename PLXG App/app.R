7baff0d62555e128d7e000fea762aca28751805b
Matthew Morgan <matt4byu@gmail.com>
2021-04-02 00:50:56 -0600

    I am a computer

--- PLXG App/app.R
+++ PLXG App/app.R
@@ -115,11 +115,11 @@ ui <- dashboardPage(
             width = 10,
             collapsible = TRUE,
             collapsed = TRUE,
-            p('This dataset was then used to build various models in an effort to predict the number of goals that would be scored by a particular team in any given match'),
+            p('The dataset was used to build various models in an effort to predict the number of goals that would be scored by a particular team in any given match'),
             p('Models were trained to minimize the Root Mean Square Error', strong('RMSE'), 'which in this case is:'),
             withMathJax(),
             p('$$\\mathrm{RMSE}=\\sqrt{\\frac{\\sum_{i=1}^{N}\\left(\\mathrm{Actual\\,Goals}_{i} - \\mathrm{Predicted\\,Goals}_{i}\\right)^{2}}N}$$'),
-            p('The best model', em('and the one used in this application'), 'was an Extreme Gradient Boosted', strong('XGBoost'), 'model. This model had RMSE values of approximately', strong('.3')),
+            p('The best model', em('and the one used in this application'), 'was an Extreme Gradient Boosted', strong('XGBoost'), 'model, it had RMSE values of approximately', strong('.3')),
             p('The specifics of the XGBoost model along with the other models created are in this', a('R script', href = 'https://github.com/mrmorgan17/PLXG/blob/main/PLXG_modeling.R'), 'on my GitHub profile'),
             p('The best XGBoost model was built using the 10 most important variables'), 
             p('These 10 variables were identified from an XGBoost model where all possible variables were used')
